dd218385c8d4df19a44bf1e1cb4de30cb2ab2ef5
Salahiev <92461234+Salahiev@users.noreply.github.com>
2021-10-16 10:50:36 +0500

    Add files via upload

---
+++ FirstChat/IService1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FirstChat
+{
+    // ПРИМЕЧАНИЕ. Можно использовать команду "Переименовать" в меню "Рефакторинг", чтобы изменить имя интерфейса "IService1" в коде и файле конфигурации.
+    [ServiceContract(CallbackContract =typeof(IServerChatCallback))]
+    public interface IService1
+    {
+        [OperationContract]
+        int Connect(string name);
+       
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract(IsOneWay =true)]
+        void SendMsg(string msg, int id);
+    }
+
+    public interface IServerChatCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void MsgCallback(string msg);
+    }
+}
